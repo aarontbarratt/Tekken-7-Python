87b68fc5571121bf0f2ac775ab72251ac669c4ec
Aaron Barratt <aarontbarratt@gmail.com>
2019-02-25 22:00:48 +0000

    ui changes

--- ui.py
+++ ui.py
@@ -5,8 +5,12 @@ win.title('Tekken 7 Frame Data')
 win.resizable(1, 1)
 
 
-def callback():
+def submitcall():
     print('click')
+
+
+def exitcall():
+    print('exit')
 
 
 # create frame
@@ -14,12 +18,16 @@ frame = tk.Frame(win, height=200, width=300)    # create frame with width and he
 frame.pack_propagate(0)                         # don't shrink
 frame.pack()
 
+frame2 = frame
+
 # entry field in frame
 entry = tk.Entry(frame)
 # submit button in frame
-submit = tk.Button(frame, text='submit', command=callback)
+submit = tk.Button(frame, text='submit', command=submitcall)
+exit = tk.Button(frame, text='exit', command=exitcall)
 
 entry.pack()
 submit.pack()
+
 
 win.mainloop()
