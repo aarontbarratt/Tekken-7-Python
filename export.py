3ccb8e1264ef888093b5e456638c77390131c1da
Aaron Barratt <aarontbarratt@gmail.com>
2019-02-25 16:58:21 +0000

    csv file includes header information programmatically. Currently verbose. Might be best to cut down.

--- export.py
+++ export.py
@@ -4,7 +4,6 @@ import csv
 def exportcsv(cursor, location, filename):
     with open(location +'\\'+filename+'.csv', mode='w', newline='') as exportfile:
         exportfile = csv.writer(exportfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        exportfile.writerow(['id', 'title', 'type'])
-        exportfile.writerow(cursor.description)
+        exportfile.writerow(cursor.description)     # provides header information, although very verbose
         for row in cursor:
             exportfile.writerow(row)
