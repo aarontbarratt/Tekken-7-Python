90842dcb2405b9d5d8143c90c4c36e9456cf5394
Aaron Barratt <aarontbarratt@gmail.com>
2019-02-25 14:05:28 +0000

    added trim functions in cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,3 +1,4 @@
+
 def left(string, number):
     string = str(string)
     string = string[:number]
@@ -14,4 +15,23 @@ def leftright(string, lnum, rnum):
     string = str(string)
     string = left(string, lnum)
     string = right(string, rnum)
+    return string
+
+
+def ltrim(string, number):
+    string = str(string)
+    string = string[number:]
+    return string
+
+
+def rtrim(string, number):
+    string = str(string)
+    string = string[:len(string)-number]
+    return string
+
+
+def lrtrim(string, l, r):
+    string = str(string)
+    string = string[l:]
+    string = string[:len(string) - r]
     return string
--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import pyodbc
-from cleaner import left, right, leftright
-
-driver = 'SQL Server'
-sqlserver = 'AARON-PC\\MFDOOM'  # pycharm complains with \
-db = 'invu'
+from cleaner import ltrim, rtrim
 
 
-conn = pyodbc.connect('Driver={'+driver+'};'
-                      'Server='+sqlserver+';'
-                      'Database='+db+';'
-                      'Trusted_Connection=yes;')
+def main():
+    driver = 'SQL Server'
+    sqlserver = 'AARON-PC\\MFDOOM'  # pycharm complains with \
+    db = 'invu'
+    conn = pyodbc.connect('Driver={'+driver+'};'
+                          'Server='+sqlserver+';'
+                          'Database='+db+';'
+                          'Trusted_Connection=yes;')
+    cursor = conn.cursor()
+    cursor.execute('with cte as '
+                   '( '
+                   '    select * from cats '
+                   '    union all '
+                   '    select * from dogs '
+                   ') '
+                   'select * '
+                   'from cte ')
 
-cursor = conn.cursor()
-cursor.execute('with cte as '
-               '( '
-               '    select * from cats '
-               '    union all '
-               '    select * from dogs '
-               ') '
-               'select * '
-               'from cte ')
+    for row in cursor:
+        row = str(row)
+        row = ltrim(row, 1)     # remove first character
+        row = rtrim(row, 1)     # remove second character
+        print(row)
 
-for row in cursor:
-    row = str(row)
 
-    print(row)
+main()
