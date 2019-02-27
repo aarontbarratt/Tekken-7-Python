0b4d9cd23f30dc2583caa260a286be078bbb136e
Aaron Barratt <aarontbarratt@gmail.com>
2019-02-27 12:55:03 +0000

    moved code into a function so it can be called from main()

--- ui.py
+++ ui.py
@@ -2,8 +2,37 @@ import tkinter as tk
 from export import exportToLog
 
 
-# ui events
-#   might be best to move into a different file in the future
+def createUI():
+    global win
+    # create window
+    win = tk.Tk()
+    win.title('Tekken 7 Frame Data')
+    win.geometry('500x450')
+    win.resizable(1, 1)
+
+    # sql instance label and field
+    lb = tk.Label(win, text='SQL Instance:')
+    lb.grid(column=0, row=0)
+
+    fd = tk.Entry(win)
+    fd.grid(column=1, row=0)
+
+    # database label and field
+    lb = tk.Label(win, text='Database')
+    lb.grid(column=0, row=1)
+
+    fd = tk.Entry(win)
+    fd.grid(column=1, row=1)
+
+    # connect button
+    bt = tk.Button(win, text='connect', command=connectSQL)
+    bt.grid(column=0, row=2)
+
+    # exit button
+    bt = tk.Button(win, text='exit', command=exitProgram)
+    bt.grid(column=1, row=2)
+
+    win.mainloop()
 
 
 def connectSQL():
@@ -14,34 +43,3 @@ def connectSQL():
 def exitProgram():
     exportToLog('exit app')
     win.destroy()   # needs () to work
-
-
-# create window
-win = tk.Tk()
-win.title('Tekken 7 Frame Data')
-win.geometry('500x450')
-win.resizable(1, 1)
-
-# sql instance label and field
-lb = tk.Label(win, text='SQL Instance:')
-lb.grid(column=0, row=0)
-
-fd = tk.Entry(win)
-fd.grid(column=1, row=0)
-
-# database label and field
-lb = tk.Label(win, text='Database')
-lb.grid(column=0, row=1)
-
-fd = tk.Entry(win)
-fd.grid(column=1, row=1)
-
-# connect button
-bt = tk.Button(win, text='connect', command=connectSQL)
-bt.grid(column=0, row=2)
-
-# exit button
-bt = tk.Button(win, text='exit', command=exitProgram)
-bt.grid(column=1, row=2)
-
-win.mainloop()
