0d6de7d366cc1384854f24f27b608d9a1de63809
Aaron Barratt <aarontbarratt@gmail.com>
2019-02-27 16:26:39 +0000

    moved all code of main. Using main to simply drop into the program and init the UI

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-from ui import *
-from pagereader import requestPage
-from export import exportToLog
-
-characters = ['akuma', 'alisa', 'anna', 'armor-king', 'asuka', 'bob', 'bryan', 'claudio', 'devil-jin', 'dragunov',
-              'eddy', 'eliza', 'feng', 'geese', 'gigas', 'heihachi', 'hwoarang', 'jack7', 'jin', 'josie', 'katarina',
-              'kazumi', 'kazuya', 'lars', 'lei', 'law', 'lee', 'leo', 'lili', 'lucky-chloe', 'marduk', 'master-raven',
-              'miguel', 'nina', 'noctis', 'paul', 'shaheen', 'steve', 'xiaoyu', 'yoshimitsu']
+from ui import createUI
 
 
 def main():
-    # createUI()
+    createUI()
     # rest of the code doesn't run when the UI is created. Probably need to use the mainloop() in UI to make any calls
-    for character in characters:
-        page = 'http://rbnorway.org/'+character+'-t7-frames/'
-        link = page
-        page = requestPage(page)
-        page = str(page)
-        exportToLog('processing: '+link)
-        print(page)
 
 
 main()
