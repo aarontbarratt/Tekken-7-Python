3273d7dda914d34f904d3687dee2bf29f845abc9
Aaron Barratt <aarontbarratt@gmail.com>
2019-02-27 00:35:55 +0000

    created pagereader.py. requestPage accepts a web link and returns the html contents as a string.

---
+++ pagereader.py
@@ -0,0 +1,31 @@
+import urllib.request
+from export import *
+from cleaner import leftright
+
+wiki = 'https://tekken.fandom.com/wiki/'
+charactersWiki = ['Bryan_Fury', 'Yoshimitsu']
+
+
+# returns html in single line
+def requestPage(webpage):
+    w = webpage
+    w = urllib.request.urlopen(w)
+    w = w.read()
+    return w
+
+
+# for each character request character wiki page
+def main():
+    for character in charactersWiki:
+        print(wiki+character)
+        page = requestPage(wiki+character)
+        page = str(page)
+        page = leftright(page, page.find('<aside '), page.find('</aside>'))
+        page = page.replace('\\n\\n', '')
+        page = page.replace('\\n', '\n')
+        page = page.replace('\\t', '  ')
+        page = page.replace('<br />', '\n')
+        exportToFile(page, 'C:\\temp', character, '.html')
+
+
+main()
