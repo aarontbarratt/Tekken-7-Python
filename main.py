64892c3675b19aa1c37e33e33cbb0dd1a2484788
Aaron Barratt <aarontbarratt@gmail.com>
2019-02-27 12:53:39 +0000

    main can call the ui into action. Doesn't currently work. reworked to use functions instead of running all code in main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-from cleaner import cleanTable
-from export import createCsv
+from ui import *
+from pagereader import requestPage
+from export import exportToLog
 
 characters = ['akuma', 'alisa', 'anna', 'armor-king', 'asuka', 'bob', 'bryan', 'claudio', 'devil-jin', 'dragunov',
               'eddy', 'eliza', 'feng', 'geese', 'gigas', 'heihachi', 'hwoarang', 'jack7', 'jin', 'josie', 'katarina',
@@ -9,18 +9,15 @@ characters = ['akuma', 'alisa', 'anna', 'armor-king', 'asuka', 'bob', 'bryan', '
 
 
 def main():
-    for x in range(len(characters)):
-        page = urllib.request.urlopen('http://rbnorway.org/'+str(characters[x])+'-t7-frames/')
-        print(characters[x])
-        page = str(page.read())
-        page = cleanTable(page, characters[x])
-        createCsv(page, characters[x], True)
-
-
-def getCharacters():
-    for x in range(len(characters)):
-        y = characters[x]
-        print(y)
+    # createUI()
+    # rest of the code doesn't run when the UI is created. Probably need to use the mainloop() in UI to make any calls
+    for character in characters:
+        page = 'http://rbnorway.org/'+character+'-t7-frames/'
+        link = page
+        page = requestPage(page)
+        page = str(page)
+        exportToLog('processing: '+link)
+        print(page)
 
 
 main()
