8d8b8bb207c6601cea2da0ddda42ab90ba90a06f
Aaron Barratt <aarontbarratt@gmail.com>
2019-03-11 16:45:19 +0000

    Moved characters list into main()

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import test
 
+# add Negan and Julia when frame data is available
+characters = ['akuma', 'alisa', 'anna', 'armor-king', 'asuka', 'bob', 'bryan', 'claudio', 'devil-jin', 'dragunov',
+              'eddy', 'eliza', 'feng', 'geese', 'gigas', 'heihachi', 'hwoarang', 'jack7', 'jin', 'josie', 'katarina',
+              'kazumi', 'kazuya', 'lars', 'lei', 'law', 'lee', 'leo', 'lili', 'lucky-chloe', 'marduk', 'master-raven',
+              'miguel', 'nina', 'noctis', 'paul', 'shaheen', 'steve', 'xiaoyu', 'yoshimitsu']
+
 
 def main():
     # should probably make sure path exists when the program initially starts
--- ui.py
+++ ui.py
@@ -4,12 +4,7 @@ from tkinter import *
 import cleaner as clean
 import export
 import pagereader as reader
-
-# add Negan and Julia when frame data is available
-characters = ['akuma', 'alisa', 'anna', 'armor-king', 'asuka', 'bob', 'bryan', 'claudio', 'devil-jin', 'dragunov',
-              'eddy', 'eliza', 'feng', 'geese', 'gigas', 'heihachi', 'hwoarang', 'jack7', 'jin', 'josie', 'katarina',
-              'kazumi', 'kazuya', 'lars', 'lei', 'law', 'lee', 'leo', 'lili', 'lucky-chloe', 'marduk', 'master-raven',
-              'miguel', 'nina', 'noctis', 'paul', 'shaheen', 'steve', 'xiaoyu', 'yoshimitsu']
+from main import characters
 
 
 def createUI():
