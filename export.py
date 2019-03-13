86ebbe8bbfcca540a20fd90082fb7d4d42934cdb
Aaron Barratt <aarontbarratt@gmail.com>
2019-03-13 09:20:54 +0000

    Cleaned up formatting. Removed test.py

--- export.py
+++ export.py
@@ -9,7 +9,7 @@ log.basicConfig(filename=logLocation+'\\'+logFileName, level=log.DEBUG)
 
 # this exports data from a sql query cursor
 def exportcsv(cursor, location, filename):
-    with open(location +'\\'+filename+'.csv', mode='w', newline='') as exportfile:
+    with open(location + '\\' + filename+'.csv', mode='w', newline='') as exportfile:
         exportfile = csv.writer(exportfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         exportfile.writerow(cursor.description)     # provides header information, although very verbose
         for row in cursor:
